47f7c9970d925743f86446ecdb72c41659717994
gincwang <gincwang@gmail.com>
2015-12-18 03:13:20 -0800

    add 2 more routes for client resources

--- app/config/routes.js
+++ app/config/routes.js
@@ -4,14 +4,18 @@ import Main from '../components/Main.js';
 import Home from '../components/Home.js';
 import About from '../components/About.js';
 import Services from '../components/Services.js';
-import ClientResources from '../components/ClientResources.js';
+import ClientResourcesDisorders from '../components/ClientResourcesDisorders.js';
+import ClientResourcesDevelopment from '../components/ClientResourcesDevelopment.js';
 import Contact from '../components/Contact.js';
 
 export default (
 	<Route path="/" component={Main}>
 		<IndexRoute component={Home}/>
 		<Route path="services" component={Services}/>
-		<Route path="client-resources" component={ClientResources}/>
+		<Route path="client-resources">
+			<Route path="common-disorders" component={ClientResourcesDisorders} />
+			<Route path="typical-development" component={ClientResourcesDevelopment} />			
+		</Route>
 		<Route path="about" component={About}/>
 		<Route path="contact" component={Contact}/>
 	</Route>

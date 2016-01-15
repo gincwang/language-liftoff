b48288bbc0217f44c710babb3678ffe8f1355b82
gincwang <gincwang@gmail.com>
2016-01-14 19:12:55 -0800

    add minify bundle.js

---
+++ min/webpack.config-min.js
@@ -0,0 +1,1 @@
+module.exports={entry:"./app/App.js",output:{filename:"bundle.js"},module:{loaders:[{test:/\.jsx?$/,exclude:/(node_modules|bower_components)/,loader:"babel",query:{presets:["es2015","react","stage-0"]}}]}};
